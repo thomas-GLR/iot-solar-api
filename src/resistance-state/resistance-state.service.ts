daf5a8b8db0a5adde3e6b4ff43744434189ac3c3
Thomas GLR <thomasguillier@hotmail.fr>
2025-03-25 12:52:52 +0100

    Ajout du protocole dans les variables d'environnement

--- src/resistance-state/resistance-state.service.ts
+++ src/resistance-state/resistance-state.service.ts
@@ -33,12 +33,13 @@ export class ResistanceStateService {
   async sendRequestToEsp32(swithOnResistance: boolean) {
     const esp32IpAddress = process.env.ESP_IP_ADDRESS;
     const esp32Port = process.env.ESP_PORT;
+    const esp32Protocole = process.env.ESP_PROTOCOL;
 
     const parameter = swithOnResistance ? '1' : '0';
 
     try {
       return await axios.get(
-        `http://${esp32IpAddress}:${esp32Port}/resistance/${parameter}`,
+        `${esp32Protocole}://${esp32IpAddress}:${esp32Port}/resistance/${parameter}`,
       );
     } catch (error) {
       throw error;
